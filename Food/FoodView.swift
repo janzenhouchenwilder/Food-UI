0180f9e6ee5aa7bdd35f25e8386a5808658dc716
Janzen Houchen-Wilder <janzenhouchen-wilder@Janzens-MacBook-Pro.local>
2026-04-06 14:52:56 -0700

    Food view now uses the same style as home view and some others

--- Food/FoodView.swift
+++ Food/FoodView.swift
@@ -21,56 +21,89 @@ struct FoodView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                TextField("Search food...", text: $foodText)
-                    .textFieldStyle(.roundedBorder)
-                    .padding(.horizontal)
-//                    .focused($foodFieldFocused)
-                Button("Search") {
-                    Task {
-//                        foodFieldFocused = false
-                        await searchFoods()
-                    }
-                }
-                .buttonStyle(.borderedProminent)
-                if vm.isLoading {
-                    ProgressView()
-                }
-                
-                if let msg = vm.errorMessage {
-                    Text(msg)
-                        .foregroundStyle(.red)
+            ZStack {
+                // BACKGROUND
+                LinearGradient(
+                    colors: [Color.blue.opacity(0.3), Color.green.opacity(0.3)],
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+                .ignoresSafeArea()
+
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 450)
+                    .opacity(0.10)
+
+                VStack {
+                    TextField("Search food...", text: $foodText)
+                        .padding()
+                        .textFieldStyle(.roundedBorder)
+                        .background(.ultraThinMaterial)
                         .padding(.horizontal)
-                }
-                
-                List(vm.foodResult) { food in
-                    NavigationLink {
-                        FoodDetailView(food: food, onAddFood: onSelect)
-                    } label: {
-                        VStack(alignment: .leading) {
-                            Text(food.food_name)
-                                .font(.headline)
-                            
-                            if let brand = food.brand_name {
-                                Text(brand)
-                                    .font(.caption)
-                                    .foregroundStyle(.secondary)
-                            }
-                            
-                            Text(food.food_description.serving_size)
-                                .font(.caption)
-                                .foregroundStyle(.secondary)
-                                .lineLimit(2)
+//                    .focused($foodFieldFocused)
+                    
+                    Button("Search") {
+                        Task {
+//                        foodFieldFocused = false
+                            await searchFoods()
                         }
                     }
+                    .buttonStyle(.borderedProminent)
+
+                    if vm.isLoading {
+                        ProgressView()
+                            .tint(.white)
+                    }
+                    
+                    if let msg = vm.errorMessage {
+                        Text(msg)
+                            .foregroundStyle(.red)
+                            .padding(.horizontal)
+                    }
+                    
+                    List(vm.foodResult) { food in
+                        NavigationLink {
+                            FoodDetailView(food: food, onAddFood: onSelect)
+                        } label: {
+                            VStack(alignment: .leading) {
+                                Text(food.food_name)
+                                    .font(.headline)
+                                    .foregroundStyle(.white)
+                                    .lineLimit(1)
+                                
+                                if let brand = food.brand_name {
+                                    Text(brand)
+                                        .font(.caption)
+//                                        .foregroundStyle(.green.opacity(0.8))
+                                        .foregroundStyle(Color(red: 0.0, green: 0.8, blue: 0.3))
+                                }
+                                
+                                Text(food.food_description.serving_size)
+                                    .font(.caption)
+                                    .foregroundStyle(Color.green.opacity(0.7))
+                                    .lineLimit(2)
+                            }
+                            .padding(4)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .frame(height: 80)
+                            .background(.ultraThinMaterial)
+                            .cornerRadius(12)
+                        }
+                        .listRowBackground(Color.clear)
+                    }
+                    .scrollDismissesKeyboard(.interactively)
+                    .scrollContentBackground(.hidden)
+                    .listRowSpacing(10)
                 }
-                .scrollDismissesKeyboard(.interactively)
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
                     Button("Close") {
                         dismiss()
                     }
+                    .foregroundStyle(.white)
                 }
             }
         }
